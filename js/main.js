07d91c53c7fe32a13f74b3ead63c6141f7db8d8f
Adrian Pearman <adrianpearman12@gmail.com>
2017-04-30 19:43:23 -0400

    complete

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
+$('.switch').on('click', function(){
+  $(this).toggleClass('on');
+  $(this).toggleClass('off');
+  $('body').toggleClass('light');
+  $('body').toggleClass('dark');
 
-
+  if ($(this).hasClass('on')){
+    $('.status').text('It\'s so bright in here!')
+  }else{
+    $('.status').text('Hey who turned off the lights!?')
+  }
+});
+// hghgh
